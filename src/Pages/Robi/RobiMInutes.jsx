c7aad0dd020b27db4c5b4d4a11b3c6aece2fdba7
Rana <ariyanmahmud02019@gmail.com>
2024-06-01 23:16:56 +0600

    Robi done

--- src/Pages/Robi/RobiMInutes.jsx
+++ src/Pages/Robi/RobiMInutes.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import MakeOffer from '../../assets/MakeOffer/MakeOffer'
+import { AirtleTbodyData, AirtleTheadData } from '../../assets/TableData'
 
 export default function RobiMInutes() {
   return (
-    <div>RobiMInutes</div>
+    <div>
+         <div className='my-5'>
+      <h5>Robi Minutes</h5>
+    </div>
+
+    <div className="table py-5 px-3 shadow-lg shadow-gray-500 w-full">
+    <MakeOffer thead={AirtleTheadData} tbody={AirtleTbodyData} />
+    </div>
+    </div>
   )
 }
