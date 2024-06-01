246ae962fe413b3e6cb95bd19b3a79a3c6804931
Rana <ariyanmahmud02019@gmail.com>
2024-06-01 23:15:09 +0600

    Telitalk done

--- src/Pages/Grameen/GpMinutes.jsx
+++ src/Pages/Grameen/GpMinutes.jsx
@@ -6,7 +6,7 @@ export default function GpMinutes() {
   return (
     <div>
          <div className='my-5'>
-           <h5>GrammenPhone Minutes</h5>
+           <h5>GrameenPhone Minutes</h5>
         </div>
 
         <div className="table py-5 px-3 shadow-lg shadow-gray-500 w-full">
--- src/Pages/Telitalk/TelData.jsx
+++ src/Pages/Telitalk/TelData.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import MakeOffer from '../../assets/MakeOffer/MakeOffer'
+import { AirtleTbodyData, AirtleTheadData } from '../../assets/TableData'
 
 export default function TelData() {
   return (
-    <div>TelData</div>
+    <div>
+       <div className='my-5'>
+        <h5>Telitalk Data</h5>
+      </div>
+
+      <div className="table py-5 px-3 shadow-lg shadow-gray-500 w-full">
+         <MakeOffer thead={AirtleTheadData} tbody={AirtleTbodyData} />
+      </div>
+    </div>
   )
 }
